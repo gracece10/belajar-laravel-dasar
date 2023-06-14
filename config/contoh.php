075f13b06c10e43f413c9136bc93d2edce7c09e9
gracece10 <gresce10@gmail.com>
2023-06-14 18:41:11 +0800

    setup project laravel

---
+++ config/contoh.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "name" => [
+        "first" => env('NAME_FIRST',"Grace"),
+        "last" => env('NAME_LAST',"Amianie")
+    ],
+    "email" => "echo.gresce10@gmail.com",
+    "web" => "https://www.programmerzamannow.com"
+    ];
