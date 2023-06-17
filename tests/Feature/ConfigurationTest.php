7b8e1b1b05288c298f6b2eec246b844cd23e97cf
gracece10 <gresce10@gmail.com>
2023-06-17 09:16:43 +0800

    setup project laravel

--- tests/Feature/ConfigurationTest.php
+++ tests/Feature/ConfigurationTest.php
@@ -15,8 +15,8 @@ class ConfigurationTest extends TestCase
         $email = config('contoh.email');
         $web = config('contoh.web');
 
-        self::assertEquals('Grace',$firstName);
-        self::assertEquals('Amianie',$lastName);
+        self::assertEquals('Grace', $firstName);
+        self::assertEquals('Amianie', $lastName);
         self::assertEquals('echo.gresce10@gmail.com',$email);
         self::assertEquals('https://www.programmerzamannow.com',$web);
     }
